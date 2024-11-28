d94c26bcd5dd935b1b24b298aa1c3acfd30e9642
jasondoan <doananhtuan22111996@gmail.com>
2024-11-28 23:42:55 +0700

    feat: integrate feature home and migrate to kotlin 2xx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,72 +1,33 @@
-import org.gradle.kotlin.dsl.support.kotlinCompilerOptions
+import vn.finance.buildSrc.Configs
 
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
-    id("kotlin-kapt")
-    alias(libs.plugins.androidHilt)
-//    alias(libs.plugins.kotlin.compose)
+    vn.core.plugins.androidApplication
 }
 
 android {
-    namespace = Configs.namespace
-    compileSdk = Configs.compileSdk
+    namespace = Configs.NAMESPACE
 
     defaultConfig {
-        applicationId = Configs.applicationId
-        minSdk = Configs.minSdk
-        targetSdk = Configs.targetSdk
-        versionCode = Configs.versionCode
-        versionName = Configs.versionName
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = Configs.javaVersion
-        targetCompatibility = Configs.javaVersion
-    }
-    kotlinOptions {
-        jvmTarget = Configs.jvmTarget
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = Configs.kotlinCompilerExtensionVersion
+        applicationId = Configs.APPLICATION_ID
+        versionCode = Configs.VERSION_CODE
+        versionName = Configs.VERSION_NAME
     }
 }
 
 dependencies {
-    implementation(fnlibs.financeTheme)
-    implementation(fnlibs.financeLaunch)
-    implementation(fnlibs.financeNavigation)
-    implementation(fnlibs.financeOnboarding)
-    implementation(fnlibs.financeAuthentication)
-    implementation(fnlibs.financeAuthenticationBusiness)
-    implementation(fnlibs.financeHome)
-    implementation(fnlibs.financeHomeBusiness)
+    implementation(libs.coreCompose)
+    implementation(libs.coreDomain)
+    implementation(libs.coreData)
 
-    implementation(libs.bundles.coreAndroidComponents)
-    implementation(platform(libs.androidxComposeBom))
-    implementation(libs.bundles.jetpackComposeComponents)
-    implementation(libs.androidxHilt)
-    kapt(libs.androidxHiltCompiler)
-    testImplementation(libs.bundles.testComponents)
-    testImplementation(libs.bundles.composeTestComponents)
-    androidTestImplementation(libs.bundles.androidTestComponents)
+    implementation(libs.financeTheme)
+    implementation(libs.financeLaunch)
+    implementation(libs.financeNavigation)
+    implementation(libs.financeOnboarding)
+    implementation(libs.provideNetworking)
 
-    implementation(fnlibs.ohteepee)
-}
+    implementation(libs.financeAuthenticationPresentation)
+    implementation(libs.financeAuthenticationBusiness)
 
-kapt {
-    correctErrorTypes = true
+    implementation(libs.financeHomePresentation)
+    implementation(libs.financeHomeBusiness)
 }
--- buildSrc/src/main/java/vn/finance/buildSrc/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package vn.finance.buildSrc
-
-class MyClass {
-}
