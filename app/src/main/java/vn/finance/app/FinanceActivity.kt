8b2a307556ef09706fc6e563d562c9380e6520a0
Jason Doan <35907749+doananhtuan22111996@users.noreply.github.com>
2024-12-15 00:10:39 +0700

    feat: Integrate setting feature with dark mode toggle functionality. Add FinanceViewModel to manage dark mode state using MutableStateFlow. Update FinanceActivity to inject SettingApi and observe dark mode changes. Modify navigation to include SettingPage and handle dark mode toggle. Update dependencies to include financeSetting modules. (#7)

--- app/src/main/java/vn/finance/app/FinanceActivity.kt
+++ app/src/main/java/vn/finance/app/FinanceActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import vn.finance.api.SettingApi
 import vn.finance.authentication.api.AuthenticationApi
 import vn.finance.home.api.HomeApi
 import vn.finance.onboarding.api.OnboardingApi
@@ -29,13 +33,19 @@ class FinanceActivity : ComponentActivity() {
     @Inject
     lateinit var homeApi: HomeApi
 
+    @Inject
+    lateinit var settingApi: SettingApi
+
+    private val viewModel: FinanceViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
+            val isDarkMode by viewModel.isDarkMode.collectAsStateWithLifecycle()
             val navController = rememberNavController()
-            AppTheme {
+            AppTheme(darkTheme = isDarkMode) {
                 FinanceNavHost(navController)
             }
         }
@@ -63,7 +73,16 @@ class FinanceActivity : ComponentActivity() {
                 })
             }
             composable(homeApi.path) {
-                homeApi.HomePage()
+                homeApi.HomePage(onGotoSetting = {
+                    navController.navigate(settingApi.path)
+                })
+            }
+            composable(settingApi.path) {
+                settingApi.SettingPage(goBack = {
+                    navController.popBackStack()
+                }, onDarkModeChanged = {
+                    viewModel.onDarkModeChanged(it)
+                })
             }
         }
     }
